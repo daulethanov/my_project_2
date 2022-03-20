668e6b94f32f4e4f69090ec7e36d1f897a6d6428
alikh <aliz1233773@gmail.com>
2022-03-20 15:08:44 +0600

    3

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -1,25 +1,40 @@
 import {Card} from "react-bootstrap";
-import React from "react";
+import React, {useState, useEffect} from "react";
+import axios from "axios";
 
-const Cards = () => {
+
+function Cards(){
+    const [product, setProduct] = useState([])
+
+    useEffect(() => {
+        axios({
+            method: "GET",
+            url: "http://127.0.0.1:8000/api/"
+        }).then(response =>
+        {
+            setProduct(response.data)
+        })
+    }, )
 
     return(
         <div className="content">
             <h1>Вся одежда</h1>
 
-            <Card style={{width: '18rem'}}>
-                <Card.Body>
-                    <Card.Title>Card Title</Card.Title>
-                    <Card.Subtitle className="mb-2 text-muted">Card Subtitle</Card.Subtitle>
-                    <Card.Text>
-                        Some quick example text to build on the card title and make up the bulk of
-                        the card's content.
-                    </Card.Text>
-                    <Card.Link href="#">Card Link</Card.Link>
-                    <Card.Link href="#">Another Link</Card.Link>
-                </Card.Body>
-            </Card>
+                    {product.map(p=> (
+                        <Card style={{width: '18rem'}} key={p.id}>
+                        <Card.Body>
+                        <Card.Title>{p.title}</Card.Title>
+                        <Card.Subtitle className="mb-2 text-muted">{p.price}</Card.Subtitle>
+                        <Card.Text>
+                            Some quick example text to build on the card title and make up the bulk of
+                            the card's content.
+                        </Card.Text>
+                        <Card.Link href="#">{p.description}</Card.Link>
+                        <Card.Link href="#">Another Link</Card.Link>
+                        </Card.Body>
+                        </Card>
+                        ))}
         </div>
-    );
+            );
 }
 export default Cards;
